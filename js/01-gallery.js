8e19211b3f16557e473a3d577e0ac24f2ed1a2c4
przystanwrozmyslaniu <iov220291@gmail.com>
2023-08-06 15:52:19 +0200

    02-lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,7 +7,7 @@ function createMarkup(arr) {
     return arr.map(({ preview, original, description }) => 
 `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}" width="340">
+      <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
     </a>
 </li>`).join('')
 }
