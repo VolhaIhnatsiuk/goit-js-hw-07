7228190032ae60bcd29486845c0c3f62bdfa0fac
przystanwrozmyslaniu <iov220291@gmail.com>
2023-08-07 21:56:47 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -15,20 +15,46 @@ function createMarkup(arr) {
 container.insertAdjacentHTML('beforeend', createMarkup(galleryItems))
 container.addEventListener("click", handlerGalleryClick);
 
-function handlerGalleryClick(evt) {
-    evt.preventDefault();
-    if (evt.target === evt.currentTarget) {
-        return;
-    }
-    // console.log(evt.target);
-    const currentImgUrl = evt.target.dataset.source;
-    const instance = basicLightbox.create(`
-<img src="${currentImgUrl}" alt="" width="1280" height="auto"/>`)
-    instance.show();
+// function handlerGalleryClick(evt) {
+//     evt.preventDefault();
+//     if (evt.target === evt.currentTarget) {
+//         return;
+//     }
+//     // console.log(evt.target);
+    
+// //     const currentImgUrl = evt.target.dataset.source;
+// //     const instance = basicLightbox.create(`
+// // <img src="${currentImgUrl}" alt="" width="1280" height="auto"/>`)
+// //     instance.show();
 
-    container.addEventListener('keydown', (evt) => {
-        if (evt.code === "Escape") {
-            instance.close();
-        }
-    })
+// //     container.addEventListener('keydown', (evt) => {
+// //         if (evt.code === "Escape") {
+// //             instance.close();
+// //         }
+// //     })
+// }
+
+const instance = basicLightbox.create(
+  `<img width="1280" height="auto" src="">`,
+  {
+    onShow: (instance) => {
+      document.addEventListener('keydown', onEscKeyPress);
+    },
+    onClose: (instance) => {
+      document.removeEventListener('keydown', onEscKeyPress);
+    },
+  }
+);
+
+function handlerGalleryClick(evt) {
+  evt.preventDefault();
+  const datasetSource = evt.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector('img').src = datasetSource;
+  instance.show();
+}
+
+function onEscKeyPress(evt) {
+  if (evt.code !== 'Escape') return;
+  instance.close();
 }
